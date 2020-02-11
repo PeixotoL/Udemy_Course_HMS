6c1854aa1490de2d2d46f301fc55e910aae13efa
Peixoto <laianepeixoto1@gmail.com>
2020-02-11 17:06:31 +0000

    commented unused static main in Patient class

--- Patient.java
+++ Patient.java
@@ -3,8 +3,8 @@ package com.semanticsquare.HMS;
 
 class Patient extends User{
 	private long patientId;
-	
-	
+
+
 	public long getPatientId(){
 		return patientId;
 	}
@@ -12,19 +12,19 @@ class Patient extends User{
 		this.patientId = patientId;
 	}
 
-	
+/**
 	public static void main(String[] args){
-		
+
 		HealthInsurancePlan insurancePlan = new PlatinumPlan();					//The SuperClass is HealthInsurancePlan!!!
 		Patient patient = new Patient();
 		patient.setInsurancePlan(insurancePlan);
-		
+
 		double[] payments = Billing.computePaymentAmount(patient, 1000.0);
-		
+
 		System.out.println("InsurancePlan: " + patient.getInsurancePlan());
-		System.out.println("plan insurance payment " + payments[0]);		
-		System.out.println("patient payment " + payments[1]);	
+		System.out.println("plan insurance payment " + payments[0]);
+		System.out.println("patient payment " + payments[1]);
 	}
-		
-	
+*/		
+
 }
