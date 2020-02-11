1d7b17cf9d45925d585332c666cc341a2945d04e
Peixoto <laianepeixoto1@gmail.com>
2020-02-11 16:06:56 +0000

    Adding InsuranceBrand Method to HealthInsurancePlan class

--- HealthInsurancePlan.java
+++ HealthInsurancePlan.java
@@ -1,14 +1,12 @@
 package com.semanticsquare.HMS;
 
-
-
 abstract class HealthInsurancePlan{
     private double coverage;
-		
+
 	public double getCoverage(){
 		return coverage;
 	}
-	
+
 	public void setCoverage(HealthInsurancePlan insurancePlan, double amount){
 		if (insurancePlan instanceof PlatinumPlan){
 			this.coverage = ((PlatinumPlan)insurancePlan).setCoverage(amount);
@@ -21,13 +19,13 @@ abstract class HealthInsurancePlan{
 		} else {
 			this.coverage = 0.0;
 		}
-		
+
 	}
 	public abstract double computeMonthlyPremium(double salary);
 
-	
+
     // Don't worry about the below code and also the InsuranceBrand class
-/**	private InsuranceBrand offeredBy;
+	private InsuranceBrand offeredBy;
 
 	public InsuranceBrand getOfferedBy() {
 		return offeredBy;
@@ -36,5 +34,5 @@ abstract class HealthInsurancePlan{
 	public void setOfferedBy(InsuranceBrand offeredBy) {
 		this.offeredBy = offeredBy;
 	}
-*/
+
 }
